2783b2161a259e38a84105685fbdb406e8be2f7f
Neil Hardman <nhardman@uk.ibm.com>
2017-11-07 14:47:30 +0000

    added project

--- release.sh
+++ release.sh
@@ -5,11 +5,12 @@
 # NRH: temporary fix to set the github userid
 GH_USER=nhardman
 GH_REMOTE=fred
+PROJECT=myrepo
 
 # remove origin remote
 git remote rm $GH_REMOTE
 #git remote add origin https://SwiftDevOps:${GH_TOKEN}@github.com/IBM-Swift/generator-swiftserver
-git remote add $GH_REMOTE https://$GH_USER:$GH_TOKEN@github.com/$GH_USER/generator-swiftserver
+git remote add $GH_REMOTE https://$GH_USER:$GH_TOKEN@github.com/$GH_USER/$PROJECT
 VERSION=`node -e "console.log(require('./package.json').version);"`
 echo "Creating release ${VERSION}" 
 git tag $VERSION && git push $GH_REMOTE $VERSION || true
@@ -32,6 +33,6 @@ npm publish
 BRANCH_TO_DELETE=updateTo$VERSION
 echo Deleting $BRANCH_TO_DELETE 
 #curl -u SwiftDevOps:${GH_TOKEN} -X DELETE -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/IBM-Swift/generator-swiftserver/branches/$BRANCH_TO_DELETE/protection"
-curl -u $GH_USER:$GH_TOKEN -X DELETE -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/$GH_USER/generator-swiftserver/branches/$BRANCH_TO_DELETE/protection"
+curl -u $GH_USER:$GH_TOKEN -X DELETE -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/$GH_USER/$PROJECT/branches/$BRANCH_TO_DELETE/protection"
 git push $GH_REMOTE :$BRANCH_TO_DELETE
 
