b8804962c8ce6cf50b0179eadadb7b7f474ed0ab
Neil Hardman <nhardman@uk.ibm.com>
2017-11-09 16:13:18 +0000

    scripts all point to nhardman/myrepo

--- release.sh
+++ release.sh
@@ -2,20 +2,15 @@
 
 # REMEMBER: this script is executed in the context of a travis maching.
 
-# NRH: temporary fix to set the github userid
-GH_USER=nhardman
-GH_REMOTE=fred
-PROJECT=myrepo
-
 # remove origin remote
-git remote rm $GH_REMOTE
+git remote rm origin
 #git remote add origin https://SwiftDevOps:${GH_TOKEN}@github.com/IBM-Swift/generator-swiftserver
-git remote add $GH_REMOTE https://$GH_USER:$GH_TOKEN@github.com/$GH_USER/$PROJECT
+git remote add origin https://nhardman:$GH_TOKEN@github.com/nhardman/myrepo
 VERSION=`node -e "console.log(require('./package.json').version);"`
 echo "Creating release ${VERSION}" 
-git tag $VERSION && git push $GH_REMOTE $VERSION || true
+git tag $VERSION && git push origin $VERSION || true
 # Merge back into develop and push those changes
-git fetch $GH_REMOTE && git checkout develop && git merge $GH_REMOTE/master && git push $GH_REMOTE develop
+git fetch origin && git checkout develop && git merge origin/master && git push origin develop
 # npm publish 
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
 npm publish
@@ -24,6 +19,6 @@ npm publish
 BRANCH_TO_DELETE=updateTo$VERSION
 echo Deleting $BRANCH_TO_DELETE 
 #curl -u SwiftDevOps:${GH_TOKEN} -X DELETE -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/IBM-Swift/generator-swiftserver/branches/$BRANCH_TO_DELETE/protection"
-curl -u $GH_USER:$GH_TOKEN -X DELETE -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/$GH_USER/$PROJECT/branches/$BRANCH_TO_DELETE/protection"
-git push $GH_REMOTE :$BRANCH_TO_DELETE
+curl -u nhardman:$GH_TOKEN -X DELETE -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/nhardman/myrepo/branches/$BRANCH_TO_DELETE/protection"
+git push origin :$BRANCH_TO_DELETE
 
--- review.sh
+++ review.sh
@@ -7,3 +7,45 @@ then
   echo This is a merge from master - exiting
   exit 0
 fi
+
+# We are processing a build resulting from merging a feature into develop
+# Proceed to create a release branch updating the version and change log
+# and a pull request from develop->master for the new release
+USER=SwiftDevOps:${GH_TOKEN}
+git config user.name "Generator bot"
+git config push.default simple
+git remote rm origin
+git remote add origin https://${USER}@github.com/nhardman/myrepo
+echo Creating branch with the new version to be reviewed
+git checkout -b temp
+echo Updating the version
+standard-version -i "CHANGES.md" --tag-version
+
+# Delete all the other branches that are release branches
+PULLS=$(curl -u ${USER} -X GET -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/nhardman/myrepo/pulls")
+LENGTH=$(echo $PULLS | jq 'length')
+SIZE=$(( $LENGTH - 1 ))
+# Loop through all the different pull requests and delete the release branches
+for i in $(seq 0 $SIZE); do
+  BASE=$(echo $PULLS | jq -r ".[$i].base.ref")
+  PULL_BRANCH=$(echo $PULLS | jq -r ".[$i].head.ref")
+  BRANCH_REGEX="updateTo[0-9]+\.[0-9]+\.[0-9]+"
+  if [[ $BASE == "master" ]] && [[ $PULL_BRANCH =~ $BRANCH_REGEX ]]; then
+    # Unprotect the branch before deleting
+    echo Removing protection on $PULL_BRANCH
+    curl -u ${USER} -X DELETE -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/nhardman/myrepo/branches/$PULL_BRANCH/protection"
+    echo Deleting $PULL_BRANCH
+    git push origin :$PULL_BRANCH
+  fi
+done
+
+VERSION=`node -e "console.log(require('./package.json').version);"`
+BRANCH=updateTo${VERSION}
+git branch -m $BRANCH
+git push -u origin $BRANCH
+# Protect the branch from force push and deletion
+curl -u ${USER} -X PUT -H "Content-Type: application/json" -H "Accept: application/vnd.github.loki-preview+json" -d "{\"required_status_checks\": {\"strict\": true,\"contexts\": []},\"required_pull_request_reviews\":{},\"restrictions\": null,\"enforce_admins\": true}" "https://api.github.com/repos/nhardman/myrepo/branches/$BRANCH/protection"
+
+# create a pull request to master
+curl -u ${USER} -H "Content-Type: application/json" -X POST -d "{\"title\": \"Release ${VERSION}\", \"head\": \"${BRANCH}\", \"base\": \"master\", \"body\": \"Do NOT attempt to push further changes to this branch.\"}" https://api.github.com/repos/nhardman/myrepo/pulls
+
